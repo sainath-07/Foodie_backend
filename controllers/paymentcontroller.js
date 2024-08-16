8d9281793de8fc3f1d034434f9de4ea204b68a1a
sainath-07 <sainathb308@gmail.com>
2024-08-16 14:12:14 +0530

    16-09-2024

--- controllers/paymentcontroller.js
+++ controllers/paymentcontroller.js
@@ -12,47 +12,34 @@ const generateOrderId = () => {
     const uniqueId = crypto.randomBytes(16).toString('hex')
     const hash = crypto.createHash('sha256')
     hash.update(uniqueId);
-
     const orderId = hash.digest('hex')
-
     return orderId.substr(0, 12);
-
-
 }
 
 const paymentfun = async (req, res) => {
 
+    const { order_amount, customer_details } = req.body
 
     try {
-
         let request = {
-            "order_amount": 1.00,
+            "order_amount": order_amount,
             "order_currency": "INR",
             "order_id": await generateOrderId(),
-            "customer_details": {
-                "customer_id": "sainath07",
-                "customer_phone": "9010995323",
-                "customer_name": "sainath",
-                "customer_email": "sainath@gmail.com"
-            }
+            "customer_details": customer_details
         }
 
         Cashfree.PGCreateOrder('2023-08-01', request).then(response => {
-            // console.log(response.data,"paymentfun")
+            console.log(response.data, "paymentfun")
             res.json(response.data)
         })
-
-
-
     } catch (error) {
-        console.log(error);
-
+        console.log(error,"paymentfunction");
+        res.json(error)
     }
 }
 
 const paymentVerify = async (req, res) => {
     try {
-
         let { orderId } = req.body;
         console.log(orderId, 'orderid')
         Cashfree.PGOrderFetchPayments('2023-08-01', orderId).then((response) => {
@@ -61,7 +48,8 @@ const paymentVerify = async (req, res) => {
             console.log(error.response.data.message, "paymentVerify")
         })
     } catch (error) {
-        console.log(error)
+        console.log(error,'paymentverify')
+        res.json(error)
     }
 }
 
--- routes/paymentRouters.js
+++ routes/paymentRouters.js
@@ -2,8 +2,8 @@ const { Router } = require('express')
 const { paymentfun, paymentVerify } = require('../controllers/paymentcontroller')
 const router = Router()
 
-router.get('/payment',paymentfun)
-    .post('/verify',paymentVerify)
+router.post('/payment', paymentfun)
+    .post('/verify', paymentVerify)
 
 
-module.exports=router
+module.exports = router
